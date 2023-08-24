365d1299e1dcda7ba780d732b0420b9d1dd6ab64
Felipe Silva <felipe.silva@spsp.com.br>
2023-08-24 16:29:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+let count = 1;
+
+// Function to make the GET request
+const fetchData = async () => {
+    try {
+        const response = await axios.get('https://www.spsp.com.br/hosp/GL/agente-transporte/notificar.php?count=' + count);
+        console.log('Response:', response.data);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+};
+
+// Call fetchData initially
+fetchData();
+
+// Set interval to call fetchData every 5 minutes (300,000 milliseconds)
+setInterval(fetchData, 300000);
